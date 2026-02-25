7f76eb8a1730d5067517eb6c4a3870bbfc05d0c4
borislav-dimov <bobolator11@gmail.com>
2026-02-25 20:36:43 +0200

    fix port for hosting

--- app.py
+++ app.py
@@ -606,5 +606,5 @@ def analyze_image():
 if __name__ == '__main__':
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000))
+        port=int(os.environ.get("PORT", 10000))
     )
